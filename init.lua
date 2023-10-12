a3729ac1d49eb6d0c471c37aceb8aef601376b9d
Guillaume Lagorce <guillaume@hey-gul.com>
2023-10-12 20:59:08 +0200

    update config with community plugins

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ return {
 
   -- Set colorscheme to use
   colorscheme = "nightfly",
+  colorscheme = "catppuccin-latte",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,7 @@
 return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.motion.leap-nvim" },
